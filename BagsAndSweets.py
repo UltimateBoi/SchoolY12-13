1126faf6dedd4b72acfa48f4a0791dc5ba44c3c4
Ultimate <63293118+UltimateBoi@users.noreply.github.com>
2024-09-27 13:56:53 +0000

    Refactor BagsAndSweets.py to simplify input handling and improve code readability

--- BagsAndSweets.py
+++ BagsAndSweets.py
@@ -1,18 +1,8 @@
-bags = int(input("Enter the number of paper bags: "))
-sweets = int(input("Enter the number of sweets: "))
-
 while True:
+    bags, sweets = int(input("Enter the number of paper bags: ")), int(input("Enter the number of sweets: "))
     if sweets <= bags:
         print("Error: The number of sweets must be greater than the number of bags.")
     else:
-        if (sweets - bags) % 2 == 0:
-            print("Yes, it is possible to put an odd number of sweets in each bag.")
-        else:
-            print("No, it is not possible to put an odd number of sweets in each bag.")
-    
-    continue_input = input("Do you want to enter another set of values? (y/n): ").strip().lower()
-    if continue_input != 'y':
+        print("Yes, it is possible to put an odd number of sweets in each bag." if (sweets - bags) % 2 == 0 else "No, it is not possible to put an odd number of sweets in each bag.")
+    if input("Do you want to enter another set of values? (y/n): ").strip().lower()[0] != 'y':
         break
-    
-    bags = int(input("Enter the number of paper bags: "))
-    sweets = int(input("Enter the number of sweets: "))
