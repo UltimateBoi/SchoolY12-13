8af5e342ec03803e6bdf663c39260bdc183410b8
Ultimate <63293118+UltimateBoi@users.noreply.github.com>
2024-10-04 13:02:35 +0000

    Refactor BagsAndSweets.py and OddEven.py for code readability and consistency

--- BagsAndSweets.py
+++ BagsAndSweets.py
@@ -1,8 +1,5 @@
 while True:
     bags, sweets = int(input("Enter the number of paper bags: ")), int(input("Enter the number of sweets: "))
-    if sweets <= bags:
-        print("Error: The number of sweets must be greater than the number of bags.")
-    else:
-        print("Yes, it is possible to put an odd number of sweets in each bag." if (sweets - bags) % 2 == 0 else "No, it is not possible to put an odd number of sweets in each bag.")
-    if input("Do you want to enter another set of values? (y/n): ").strip().lower()[0] != 'y':
-        break
+    if sweets <= bags: print("Error: The number of sweets must be greater than the number of bags.")
+    else: print("Yes, it is possible to put an odd number of sweets in each bag." if (sweets - bags) % 2 == 0 else "No, it is not possible to put an odd number of sweets in each bag.")
+    if input("Do you want to enter another set of values? (y/n): ").strip().lower()[0] != 'y': break 
---
+++ OddEven.py
@@ -0,0 +1,16 @@
+class Num:
+    def __init__(self):
+        return
+    def isEven(num):
+        if num % 2 == 0: return True
+        else: return False
+    def isOdd(num):
+        if num % 2 != 0: return True
+        else: return False
+
+def is_odd(num):
+    if num % 2 == 0: return False 
+    else: return True
+def is_even(num):
+    if num % 2 == 0: return True
+    else: return False
